b13b98f1f278262a887f66ab7f2c75d1da2f4e31
Tina Huynh Le <huynhletina@gmail.com>
2024-02-05 17:50:36 +0100

    fix errors, render layout correctly

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,5 +1,12 @@
 import Navigation from "../Navigation/Navigation";
 
-export default function Layout() {
-  return <Navigation />;
+export default function Layout({ children }) {
+  return (
+    <>
+      <div>
+        <Navigation />
+        <div>{children}</div>
+      </div>
+    </>
+  );
 }
