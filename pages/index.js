a8cb1d54e6979b1e7a7d5d6755a43a3c9b362b71
Tina Huynh Le <huynhletina@gmail.com>
2024-02-05 12:52:25 +0100

    create ArtPiecePreview component, render list with art pieces

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,29 @@
 import useSWR from "swr";
+import ArtPiecePreview from "@/components/ArtPiecePreview";
 
 export default function ArtPieces({ pieces }) {
-  const { data } = useSWR("https://example-apis.vercel.app/api/art");
-  return <h1>Art pieces to come</h1>;
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art"
+  );
+
+  if (error) return <h1>failed to load</h1>;
+  if (isLoading) return <h1>loading...</h1>;
+
+  return (
+    <>
+      <ul>
+        {data.map((piece) => (
+          <li key={piece.slug}>
+            <ArtPiecePreview
+              image={piece.imageSource}
+              title={piece.title}
+              artist={piece.artist}
+              height={piece.dimensions.height}
+              width={piece.dimensions.width}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
