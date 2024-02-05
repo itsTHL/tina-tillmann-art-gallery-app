752cad13a9f2a091cc7907bf8df976b4efd21a30
Tillmann Severin <tillmann.severin@posteo.de>
2024-02-05 17:49:59 +0100

    add art-pieces page

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
-import useSWR from "swr";
-import ArtPieces from "@/components/ArtPieces";
 import Spotlight from "../components/Spotlight/Spotlight";
 
-export default function Homepage({ pieces }) {
-  const { data, error, isLoading } = useSWR(
-    "https://example-apis.vercel.app/api/art"
-  );
-
-  if (error) return <h1>failed to load</h1>;
-  if (isLoading) return <h1>loading...</h1>;
-
+export default function SpotlightPage({ data }) {
+  console.log("Data ---->", data);
   return (
     <>
       <h1>Art Gallery</h1>
-      <Spotlight />
-      <ArtPieces pieces={data} />
+      <Spotlight data={data} />
     </>
   );
 }
