7c772e9d6866f6c0383dc09ae8dcf93f8a4d1409
Tillmann Severin <tillmann.severin@posteo.de>
2024-02-05 14:20:35 +0100

    setup for test

--- components/Spotlight/Spotlight.test.js
+++ components/Spotlight/Spotlight.test.js
@@ -1,5 +1,15 @@
+// import Spotlight from ".";
+
+// test("renders one art piece", () => {
+//     render(<Image />);
+//     const heading = screen.getByRole("heading", {
+//       name: /temperature unit converter/i,
+//     });
+//     expect(heading).toBeInTheDocument();
+//   });
+
 // Acceptance Criteria
 
-//     One art piece is picked at random to show as a spotlight piece
-//     The art piece image is displayed 🖼️
-//     The art piece artist is displayed 🖼️
+//     One art piece is picked at random to show as a spotlight piece => random piece function
+//     The art piece image is displayed 🖼️ => display
+//     The art piece artist is displayed 🖼️ => display
