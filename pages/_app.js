9b5d46040267b38da6c4bdaa31d979c54017c50a
Tina Huynh Le <huynhletina@gmail.com>
2024-02-06 11:30:00 +0100

    create new state with info about is favorite or not, make new state global, write toggle function for toggling is favorite

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import useSWR from "swr";
+import { useState } from "react";
 import GlobalStyle from "../styles";
 import Layout from "@/components/Layout/Layout";
 import "../components/Navigation/Navigation.css";
@@ -8,10 +9,37 @@ import "../components/ArtPieceDetails/ArtPieceDetails.css";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
+  // FETCHING DATA FROM API
   const { data, error, isLoading } = useSWR(
     "https://example-apis.vercel.app/api/art",
     fetcher
   );
+
+  // SETTING STATE FOR FAVORITE COMPONENT
+  const [artPiecesInfo, setArtPiecesInfo] = useState({ isFavorite: false });
+  console.log(artPiecesInfo);
+
+  function toggleFavorite(artPieceSlug) {
+    setArtPiecesInfo((artPiecesInfo) => {
+      // finding correct art piece
+      const artPiece = artPiecesInfo.find(
+        (artPiece) => artPiece.slug === artPieceSlug
+      );
+
+      // if art piece is already in state, toggle isFavorite property
+      if (artPiece) {
+        return artPiecesInfo.map((artPiece) =>
+          artPiece.slug === artPieceSlug
+            ? { ...artPiece, isFavorite: !artPiece.isFavorite }
+            : artPiece
+        );
+      }
+
+      // if art piece is not in state, add isFavorite key with value of true
+
+      return [...artPiecesInfo, { slug, isFavorite: true }];
+    });
+  }
 
   if (error) return <h1>failed to load</h1>;
   if (isLoading) return <h1>loading...</h1>;
@@ -20,7 +48,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Layout>
         <GlobalStyle />
-        <Component {...pageProps} data={data} />
+        <Component {...pageProps} data={data} onToggle={toggleFavorite} />
       </Layout>
     </>
   );
